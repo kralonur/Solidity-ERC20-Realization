3408e4973f7079468923b083772f057e37929318
kralonur <kralonur1998@gmail.com>
2021-12-07 20:50:40 +0700

    Create deploy script

---
+++ scripts/deploy.ts
@@ -0,0 +1,16 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const [owner] = await ethers.getSigners();
+  const Erc20Token = await ethers.getContractFactory("ERC20Token");
+  const erc20Token = await Erc20Token.deploy();
+
+  await erc20Token.deployed();
+
+  console.log("ERC20Token deployed to:", owner.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
